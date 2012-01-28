1ec72fb2f0c42d5a5704f42cbad42f60c074e8bb
Stu Rob <stumrob@gmail.com>
2012-01-28 17:52:28 +0000

    tidyup and write design files

--- nodejs/savetofile.js
+++ nodejs/savetofile.js
@@ -1,37 +1,42 @@
 #!/usr/bin/node
 
-var querystring = require('querystring');
+var querystring = require('querystring'),
+    fs          = require('fs');
 
 require("http").createServer(function(request, response) {
+  var body = '';
+  
   if (request.method != "POST") {
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     response.end(''); 
   } else {
-    var body = "";
-    var file_id = request.url.slice(1);
     request.setEncoding('utf8');
-    
     request.on('data', function (data) {
       body = body + data;
     });
 
     request.on('end', function () {
+      var clean_date    = (new Date).toISOString().replace(/:|Z|T/g,'\.').slice(0,-5),
+          file_id       = request.url.slice(1),
+          save_filename = './tmp/' + file_id + '_' + clean_date + '.json',
+          data_filename = './js/designs/' + file_id + '.json',
+          save_stream   = fs.createWriteStream( save_filename  ),
+          data_stream   = fs.createWriteStream( data_filename ),
+          output        = JSON.stringify( JSON.parse( body ), null, 2 );
+      
       response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
       response.end();
-      var date = new Date(Date.now()).toISOString().replace(/:/g,'\.');
-      var filename = './tmp/' + file_id + '_' + date + '.json';
-      var fs = require('fs');
+            
+      var writefile = function(s) {
+        console.log('writing ' + s.path );
+        s.write( output );
+        s.destroy();
+        console.log('closed' + s.path )
+      }
       
-      var stream = fs.createWriteStream( filename );
-      var tmp = JSON.parse( body );
-      
-      var output = JSON.stringify(tmp, null, 2 );
-      stream.once('open', function(fd) {
-        stream.write( output );
-        stream.destroySoon();
-      console.log('closed ' + filename);
-      });
-      console.log('wrote ' + filename);
+      save_stream.once('open', function() { writefile( save_stream ) });  
+      data_stream.once('open', function() { writefile( data_stream ) });
+
       // console.log( output );
     });
   }
